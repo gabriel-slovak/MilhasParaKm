71b1137df6a5f6ce96a3b1264014d9e75d8c6ee0
gabriel-slovak <slovakgabriellopes@gmail.com>
2021-03-18 17:24:13 -0300

    finalização do programa MilhasParaKm

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas;
+            double km = 1.609;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------");
+            Console.WriteLine("Milhas Para Km");
+            Console.WriteLine("--------------");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.Write("Digite seu valor em milhas: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            milhas = double.Parse (Console.ReadLine());
+            Console.ResetColor();
+            Console.Write("\n");
+
+            Console.Write("Seu valor de milhas para Km: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{milhas * km}");
+            Console.ResetColor();
+            Console.Write("\n");
+
+        }
+    }
+}
